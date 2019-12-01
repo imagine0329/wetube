ee9ee0e010e0c0f4f700706f68e179e33af1a272
Hyejin Im <heyjean9@gmail.com>
2019-12-02 05:45:44 +0900

    #2.25 More Controllers
    2019. 12. 01 Sun

---
+++ db.js
@@ -0,0 +1,54 @@
+export const videos = [
+  {
+    id: 100001,
+    title: "video #1",
+    description: "This is a video #1",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creators: {
+      id: 100000,
+      name: "HJ",
+      email: "hj@hjim.com"
+    }
+  },
+  {
+    id: 100002,
+    title: "video #2",
+    description: "This is a video #2",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creators: {
+      id: 100000,
+      name: "HJ",
+      email: "hj@hjim.com"
+    }
+  },
+  {
+    id: 100003,
+    title: "video #3",
+    description: "This is a video #3",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creators: {
+      id: 100000,
+      name: "HJ",
+      email: "hj@hjim.com"
+    }
+  },
+  {
+    id: 100004,
+    title: "video #4",
+    description: "This is a video #4",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creators: {
+      id: 100000,
+      name: "HJ",
+      email: "hj@hjim.com"
+    }
+  }
+];
