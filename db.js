ec4d186d2f3e745947e855017e74428259077e94
Hyejin Im <heyjean9@gmail.com>
2019-12-02 21:52:42 +0900

    #3.2 Configuring Dot Env
    2019. 12. 02 Mon

--- db.js
+++ db.js
@@ -1,54 +1,16 @@
-export const videos = [
-  {
-    id: 100001,
-    title: "video #1",
-    description: "This is a video #1",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creators: {
-      id: 100000,
-      name: "HJ",
-      email: "hj@hjim.com"
-    }
-  },
-  {
-    id: 100003,
-    title: "video #2",
-    description: "This is a video #2",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creators: {
-      id: 100000,
-      name: "HJ",
-      email: "hj@hjim.com"
-    }
-  },
-  {
-    id: 100003,
-    title: "video #3",
-    description: "This is a video #3",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creators: {
-      id: 100000,
-      name: "HJ",
-      email: "hj@hjim.com"
-    }
-  },
-  {
-    id: 100004,
-    title: "video #4",
-    description: "This is a video #4",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creators: {
-      id: 100000,
-      name: "HJ",
-      email: "hj@hjim.com"
-    }
-  }
-];
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlparser: true,
+  useFindAndModify: false
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("🍉 Connected to DB");
+const handleError = () => console.log(`🎲 Error on DB Connection:${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
