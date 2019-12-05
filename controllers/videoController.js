a410bcf5e27c27ad4b768bfb30c84007d74bf8ce
Hyejin Im <heyjean9@gmail.com>
2019-12-06 05:33:38 +0900

    #3.8 Getting Video by ID
    2019. 12. 05 Thu

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -24,17 +24,40 @@ export const search = (req, res) => {
 
 export const getUpload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
-export const postUpload = (req, res) => {
-  const { body, file } = req;
-  console.log(body, file);
+
+export const postUpload = async (req, res) => {
+  const {
+    body: { title, description },
+    file: { path }
+  } = req;
+  const newVideo = await Video.create({
+    fileUrl: path,
+    title,
+    description
+  });
+  console.log(newVideo);
   // To Do: Upload and save video
-  res.render("upload", { pageTitle: "Upload" });
-  // res.redirect(routes.videoDetail(100001));
+  res.redirect(routes.videoDetail(newVideo.id));
 };
 
-export const videoDetail = (req, res) =>
-  res.render("videoDetail", { pageTitle: "Video Detail" });
-export const editVideo = (req, res) =>
+export const videoDetail = async (req, res) => {
+  const {
+    params: { id }
+  } = req;
+
+  try {
+    const video = await Video.findById(id);
+    res.render("videoDetail", { pageTitle: "Video Detail", video });
+  } catch (error) {
+    console.log(error);
+    res.redirect(routes.home);
+  }
+};
+export const editVideo = (req, res) => {
+  const {
+    params: { id }
+  } = req;
   res.render("editVideo", { pageTitle: "Edit Video" });
+};
 export const deleteVideo = (req, res) =>
   res.render("deleteVideo", { pageTitle: "Delete Video" });
