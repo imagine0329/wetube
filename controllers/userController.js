31299bca4e766a3bd53ca0ac60058a3cce25530f
Hyejin Im <heyjean9@gmail.com>
2019-12-27 07:16:41 +0900

    #6.7 Github Log In part Two
    2019. 12. 27 Fri

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,11 @@
+import passport from "passport";
 import routes from "../routes";
 import User from "../models/User";
 
 export const getJoin = (req, res) => {
   res.render("join", { pageTitle: "Join" });
 };
-export const postJoin = async (req, res) => {
+export const postJoin = async (req, res, next) => {
   const {
     body: { name, email, password, password2 }
   } = req;
@@ -18,22 +19,34 @@ export const postJoin = async (req, res) => {
         email
       });
       await User.register(user, password);
+      next();
     } catch (error) {
       console.log(error);
+      res.redirect(routes.home);
     }
-    // To Do: Log user in
-    res.redirect(routes.home);
   }
 };
 
 export const getLogin = (req, res) =>
   res.render("login", { pageTitle: "Login" });
-export const postLogin = (req, res) => {
-  res.redirect(routes.home);
+
+export const postLogin = passport.authenticate("local", {
+  failureRedirect: routes.login,
+  successRedirect: routes.home
+});
+
+export const githubLogin = passport.authenticate("github");
+
+export const githubLoginCallback = (accessToken, refreshToken, profile, cb) => {
+  console.log(accessToken, refreshToken, profile, cb);
+};
+
+export const postGithubLogIn = (req, res) => {
+  res.send(routes.home);
 };
 
 export const logout = (req, res) => {
-  // To Do: Process logout
+  req.logout();
   res.redirect(routes.home);
 };
 
